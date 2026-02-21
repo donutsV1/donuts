26285550607c6ec83ceccfa1e7fb460d7d85cba0
donutsV1 <axel.donutsv1@gmail.com>
2026-02-21 19:11:17 +0100

    bonjour

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("toggleMode");
+const html = document.documentElement;
+
+btn.addEventListener("click", () => {
+  const estClair = html.getAttribute("data-theme") === "clair";
+  html.setAttribute("data-theme", estClair ? "sombre" : "clair");
+  btn.textContent = estClair ? "☀️ Mode clair" : "🌙 Mode sombre";
+});
