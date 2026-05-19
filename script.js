718f04860e06abb639229542ea29ccc1f6feeb07
donutsV1 <axel.donutsv1@gmail.com>
2026-05-19 18:07:28 +0200

    les bro avan tou
    les bro avan tou

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const btn = document.getElementById("toggleMode");
 const html = document.documentElement;
 
+class MyMenu extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="pagenext">
+        <a href="jeux 1 .html">jeux 1</a>
+        <a href="jeux 3d .html">jeux 3D</a>
+        <a href="poin .html">poin calcule</a>
+        <a href="sudoku.html">sudoku</a>
+        <a href="tiktacto.html">tiltacto</a>
+      </div>
+    `;
+  }
+}
+
+// On enregistre la balise personnalisée
+customElements.define('mon-menu', MyMenu);
+
+// On définit la balise personnalisée <mon-menu></mon-menu>
+customElements.define('mon-menu', MyMenu);
 btn.addEventListener("click", () => {
   const estClair = html.getAttribute("data-theme") === "clair";
   html.setAttribute("data-theme", estClair ? "sombre" : "clair");
